801b945723395992881e17390e00fcbf524e18e7
Adam Spencer <aspencer0919@gmail.com>
2020-04-06 17:06:01 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -14,9 +14,7 @@ def retrieval
                   "porcelain vases" => 2,
                   "oil paintings" => 3
                  }
-
-return shipping_manifest["oil paintings"]  # Look up the value of the "oil paintings" key in the shipping_manifest hash below
-
+return shipping_manifest["oil paintings"]
 end
 
 def adding
@@ -25,15 +23,7 @@ def adding
                   "porcelain vases" => 2,
                   "oil paintings" => 3
                  }
-  # add 2 muskets to the shipping_manifest hash below
 shipping_manifest["muskets"] = 2
 shipping_manifest["gun powder"] = 4
-
-
-  # add 4 gun powder to the shipping_manifest hash below
-
-
 return shipping_manifest
-  # return the shipping_manifest hash below
-
 end
